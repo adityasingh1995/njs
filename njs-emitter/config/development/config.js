4313197639628ded6d7aa1c3946e8f2af9afd4a5
Aditya Singh <adityasingh397@gmail.com>
2021-06-13 02:56:42 +0530

    NJS-emitter: updated config

--- njs-emitter/config/development/config.js
+++ njs-emitter/config/development/config.js
@@ -4,5 +4,20 @@ module.exports = {
 			'host': 'localhost',
 			'port': 8000
 		}
-	}]
+	}],
+	'generator': {
+		'password': '49TWW3UY4PSC3GYG',
+		'people': [
+			'Jack Reacher',
+			'Lack Preacher',
+			'Pack Bleacher'
+		],
+
+		'places': [
+			'istanbul',
+			'paris',
+			'tokyo',
+			'delhi'
+		],
+	}
 };
