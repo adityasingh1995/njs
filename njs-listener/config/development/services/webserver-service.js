49ca66ad59276a3078d1af364b4a835605835d37
Aditya Singh <adityasingh397@gmail.com>
2021-06-13 20:52:55 +0530

    config update

--- njs-listener/config/development/services/webserver-service.js
+++ njs-listener/config/development/services/webserver-service.js
@@ -3,5 +3,9 @@ module.exports = {
     'port': 8080,
     'cookeName': 'njs!cookie!',
     'sessionSecretKey': 'njs!cookie!secret!',
-    'maxAge': 3600000 * 12
+    'maxAge': 3600000 * 12,
+    'redis': {
+        'host': '127.0.0.1',
+        'port': 6379
+    }
 };
