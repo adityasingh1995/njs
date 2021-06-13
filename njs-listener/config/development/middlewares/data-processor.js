fa5f406ba408b9744af87a8f133a60b772e2b138
Aditya Singh <adityasingh397@gmail.com>
2021-06-13 14:29:26 +0530

    Njs-listener:config changes

--- njs-listener/config/development/middlewares/data-processor.js
+++ njs-listener/config/development/middlewares/data-processor.js
@@ -1,1 +1,5 @@
-module.exports = {};
+module.exports = {
+    'password': '49TWW3UY4PSC3GYG',
+    'salt': 'B0G723HKCRRVXZFJ',
+    'iv': 'G1R2GENLO0U0WWNY'
+};
--- njs-listener/config/development/services/rabbitmq-service.js
+++ njs-listener/config/development/services/rabbitmq-service.js
@@ -1,1 +1,15 @@
-module.exports = {};
+module.exports = {
+    "port": 5672,
+    "type": "amqp",
+    "vhost": "/njs",
+    "locale": "en_US",
+    "frameMax": 0,
+    "hostname": "localhost",
+    "password": "guest",
+    "protocol": "amqp",
+    "username": "guest",
+    "heartbeat": 20,
+    "exchange": "njsEx",
+    "exchangeType": "direct",
+    "prefetch": 100
+};
